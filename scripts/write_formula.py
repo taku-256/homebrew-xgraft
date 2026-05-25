8f2e1043fd25f51bff43c82f4b40d6dd2dd3716d
taku-256 <146168304+taku-256@users.noreply.github.com>
2026-05-25 13:28:45 +0900

    update python

--- scripts/write_formula.py
+++ scripts/write_formula.py
@@ -2,14 +2,21 @@ from pathlib import Path
 import os
 
 tag = os.environ["TAG"]
-sha = os.environ["SHA"]
-arch = os.environ.get("ARCH", "x86_64-apple-darwin")
+
+sha_arm = os.environ["SHA_ARM"]
+sha_x86 = os.environ["SHA_X86"]
 
 content = f'''class Xgraft < Formula
     desc "MPLAB X project tool"
     homepage "https://github.com/taku-256/xgraft"
-    url "https://github.com/taku-256/xgraft/releases/download/{tag}/xgraft-{tag}-{arch}.tar.gz"
-    sha256 "{sha}"
+
+    if Hardware::CPU.arm?
+        url "https://github.com/taku-256/xgraft/releases/download/{tag}/xgraft-{tag}-aarch64-apple-darwin.tar.gz"
+        sha256 "{sha_arm}"
+    else
+        url "https://github.com/taku-256/xgraft/releases/download/{tag}/xgraft-{tag}-x86_64-apple-darwin.tar.gz"
+        sha256 "{sha_x86}"
+    end
 
     def install
         bin.install "xgraft"
